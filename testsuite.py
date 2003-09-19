5da4113e8655f48c71a1416d62b772196fc10c49
Frederic Gobry <gobry@puck.ch>
2003-09-19 14:46:51 +0000

    forbid installation of broken recode New-files: New-directories: Removed-files: Removed-directories: Renamed-files: Renamed-directories: Modified-files: ./ChangeLog ./testsuite.py Modified-directories: New-patches: gobry@pybliographer.org--public/python-bibtex--stable--1.2--patch-6 Removed-patches:
    the test suite checks that recode is not broken.
    
    imported from: gobry@pybliographer.org--public/python-bibtex--stable--1.2--patch-6

--- testsuite.py
+++ testsuite.py
@@ -1,3 +1,29 @@
+""" Run is the main function that will check if the recode and bibtex
+modules are working """
+
+def check_recode ():
+    import _recode
+
+    # First, check if the recode version has the famous 3.6 bug
+    rq = _recode.request ('latin1..latex')
+    
+    if _recode.recode (rq, 'abc') != 'abc':
+        raise RuntimeError ('the _recode module is broken')
+
+    return
+
+
+def check_bibtex ():
+    import _bibtex
+    
+    return
+
+
 
 def run ():
-    pass
+    check_recode ()
+    check_bibtex ()
+    return
+
+
+    
