75bd990de273de58138458d35dd641d0eced3be9
Frederic Gobry <gobry@pybliographer.org>
2007-04-24 17:32:48 +0000

    merged with zoltan's branch
    Patches applied:
    
     * kota@szbk.u-szeged.hu--pyblio/python-bibtex--stable--1.2--patch-4
       merged patch-33 from Frederic
    
     * kota@szbk.u-szeged.hu--pyblio/python-bibtex--stable--1.2--patch-5
       merge patch-34 from Frederic
    
     * kota@szbk.u-szeged.hu--pyblio/python-bibtex--stable--1.2--patch-6
       ready for 1.2.4
    
    imported from: gobry@pybliographer.org--public/python-bibtex--stable--1.2--patch-35

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup, Extension, Command
 from distutils.errors import DistutilsExecError
 from distutils.command.install import install as base_install
 
-version = '1.2.3'
+version = '1.2.4'
 
 
 bibtex = [
